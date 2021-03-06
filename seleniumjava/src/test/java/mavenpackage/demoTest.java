680e885ad68febf3815b6f2184f2142c31a201df
billycrowson <billy.crowson@cgi.com>
2021-03-05 23:09:02 -0600

    Create first test

---
+++ seleniumjava/src/test/java/mavenpackage/demoTest.java
@@ -0,0 +1,23 @@
+package mavenpackage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class demoTest {
+    
+    public static void main(String[] args) throws InterruptedException {
+        
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://the-internet.herokuapp.com/");
+
+        Thread.sleep(3000);
+
+        driver.quit();;
+
+    }
+
+}
